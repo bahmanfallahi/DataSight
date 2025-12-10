c0fb91d86ca8fd31c61ef3288f0b76464e10f9d6
bahman fallahi <bahman.f.behtash@gmail.com>
2025-12-10 11:42:40 +0000

    در بخش فرتر:
    نوشته develop with ❤️ by bahman fallahi رو بیار در وسط فوت

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -108,8 +108,8 @@ export default function LandingPage() {
         </div>
       </main>
        <footer className="py-6 md:px-8 md:py-0">
-            <div className="container flex flex-col items-center justify-between gap-4 md:h-24 md:flex-row">
-              <p className="text-balance text-center text-sm leading-loose text-muted-foreground md:text-left">
+            <div className="container flex h-24 items-center justify-center">
+              <p className="text-balance text-center text-xs leading-loose text-muted-foreground">
                 develop with ❤️ by bahman fallahi
               </p>
             </div>
