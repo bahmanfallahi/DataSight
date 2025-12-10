3cec0db24efd8f46dfe9969fd2b5330728256cf0
bahman fallahi <bahman.f.behtash@gmail.com>
2025-12-10 11:54:57 +0000

    برای حل این مشکل، باید کانتینر را نسبت به پدرش (یا همان بادی) وسط‌چین کن

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -107,12 +107,12 @@ export default function LandingPage() {
 
         </div>
       </main>
-       <footer className="py-6 md:px-8 md:py-0">
-            <div className="container flex h-24 items-center justify-center">
-              <p className="text-balance text-center text-xs leading-loose text-muted-foreground">
-                develop with ❤️ by bahman fallahi
-              </p>
-            </div>
+       <footer className="w-full py-6">
+          <div className="container mx-auto flex h-24 items-center justify-center">
+            <p className="text-xs text-muted-foreground">
+              develop with ❤️ by bahman fallahi
+            </p>
+          </div>
       </footer>
     </div>
   );
